3c46e7f3825d4690cf1965c7f953a79287dc0f9f
Dwi Sulfahnur <hello@dwisulfahnur.com>
2020-04-04 21:51:08 +0700

    vendor: Add requirement packages

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xenditclient',
+    version='0.0.1',
+    packages=['tests', 'xenditclient'],
+    url='https://github.com/dwisulfahnur/python-xendit-client',
+    license='BSD 3-Clause',
+    author='Dwi Sulfahnur',
+    author_email='dwisulfahnur@gmail.com',
+    description='Xendit REST API Client for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.4',
+    install_requires=['requests>=2.3.0'],
+    tests_requires=['pytest>=3.0.6']
+)
